45a90f4d85f1761717b93e151fb0b525de9b4c61
stefanfredik <stefanfredik@gmail.com>
2023-05-04 16:26:01 +0800

    major update

--- app/Models/KuotaModel.php
+++ app/Models/KuotaModel.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
-use PhpParser\Node\Stmt\Return_;
 
 class KuotaModel extends Model
 {
@@ -16,10 +15,4 @@ class KuotaModel extends Model
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
     protected $allowedFields    = ['id', 'tahun', 'periode', 'jumlah_kuota', 'tanggal_terima', 'keterangan'];
-
-
-    // public function findAll(int $limit = 0, int $offset = 0)
-    // {
-    //     return $this->findAll()->sort();
-    // }
 }
