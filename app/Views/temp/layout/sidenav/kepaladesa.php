ed2a60d4745eca8d5d8b9f9f0e75227cc9f9cf33
stefanfredik <stefanfredik@gmail.com>
2023-05-23 00:04:37 +0800

    update sidebar

--- app/Views/temp/layout/sidenav/kepaladesa.php
+++ app/Views/temp/layout/sidenav/kepaladesa.php
@@ -19,8 +19,15 @@
         Data Penduduk
     </a>
 
-    <a class="nav-link <?= url_is('/laporan') ? 'active' : '' ?>" href="/laporan">
-        <div class="nav-link-icon"><i data-feather="hard-drive"></i></div>
-        Data Laporan
+    <a class="nav-link collapsed <?= url_is("/laporan*") ? 'active' : '' ?>" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#laporan" aria-expanded="false" aria-controls="collapseDashboards">
+        <div class="nav-link-icon"><i data-feather="activity"></i></div>
+        Laporan
+        <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
     </a>
+    <div class="collapse" id="laporan" data-bs-parent="#accordionSidenav">
+        <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
+            <a class="nav-link <?= url_is("/laporan/bantuan") ? 'active' : '' ?>" href="/laporan/bantuan">Data Bantuan</a>
+            <a class="nav-link <?= url_is("/laporan/penduduk") ? 'active' : '' ?>" href="/laporan/penduduk">Penduduk</a>
+        </nav>
+    </div>
 </div>
