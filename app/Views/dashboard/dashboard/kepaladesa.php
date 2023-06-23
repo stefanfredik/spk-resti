89ed3ac44475a47ec19d230c6b26e5540c70c8b0
stefanfredik <stefanfredik@gmail.com>
2023-06-23 08:16:48 +0800

    update dashborad

--- app/Views/dashboard/dashboard/kepaladesa.php
+++ app/Views/dashboard/dashboard/kepaladesa.php
@@ -1,20 +1,18 @@
 <div class="row">
-    <div class="col-lg-3 mb-4">
-        <!-- Dashboard example card 1-->
+    <!-- <div class="col-lg-3 mb-4">
         <a class="card lift h-100" href="/datapenduduk">
             <div class="card-body d-flex justify-content-center flex-column">
                 <div class="d-flex align-items-center justify-content-between">
                     <div class="me-3">
                         <i class="bi bi-list-ul text-primary fa-2x"></i>
-                        <h5>Data Perhitungan</h5>
+                        <h5>Data Penduduk</h5>
                         <div class="text-primary small">Data Penduduk</div>
                     </div>
-                    <!-- <img src="/sbadmin/assets/img/illustrations/browser-stats.svg" alt="..." style="width: 8rem" /> -->
                     <i class="bi bi-people-fill fa-5x text-primary"></i>
                 </div>
             </div>
         </a>
-    </div>
+    </div> -->
 
     <div class="col-lg-3 mb-4">
         <!-- Dashboard example card 3-->
