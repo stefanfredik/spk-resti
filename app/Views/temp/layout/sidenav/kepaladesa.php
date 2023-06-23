9abc6c090ff1edac4f9288d87b3e898e0d7db468
stefanfredik <stefanfredik@gmail.com>
2023-06-23 09:10:14 +0800

    fix kepaladesa sidebar

--- app/Views/temp/layout/sidenav/kepaladesa.php
+++ app/Views/temp/layout/sidenav/kepaladesa.php
@@ -14,10 +14,10 @@
     </a>
 
     <div class="sidenav-menu-heading">Data</div>
-    <a class="nav-link <?= url_is('/datapenduduk') ? 'active' : '' ?>" href="/datapenduduk">
+    <!-- <a class="nav-link <?= url_is('/datapenduduk') ? 'active' : '' ?>" href="/datapenduduk">
         <div class="nav-link-icon"><i data-feather="layers"></i></div>
         Data Penduduk
-    </a>
+    </a> -->
 
     <a class="nav-link collapsed <?= url_is("/laporan*") ? 'active' : '' ?>" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#laporan" aria-expanded="false" aria-controls="collapseDashboards">
         <div class="nav-link-icon"><i data-feather="activity"></i></div>
